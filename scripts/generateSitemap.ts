4ad157825da9910379708aa28ef1b67da953db94
opera7133 <ainznino@pm.me>
2022-09-26 00:26:14 +0900

    urlset

--- scripts/generateSitemap.ts
+++ scripts/generateSitemap.ts
@@ -4,12 +4,12 @@ const routes = ['/']
 const site_url = 'https://fes.vcborn.com'
 
 function formatPage(name: string, published: string) {
-  return `\n  <sitemap>\n    <loc>${name}</loc>\n    <lastmod>${published}</lastmod>\n  </sitemap>`
+  return `\n  <url>\n    <loc>${name}</loc>\n    <lastmod>${published}</lastmod>\n  </url>`
 }
 
 async function generateSitemap() {
   const publishDate = new Date().toISOString()
-  let content = `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="//www.nsb.com/wp-content/plugins/wordpress-seo-premium/css/main-sitemap.xsl"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
+  let content = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
   routes.forEach((route: string) => {
     content = `${content}${formatPage(`${site_url}${route}`, publishDate)}`
   })
@@ -20,7 +20,7 @@ async function generateSitemap() {
     content = `${content}${formatPage(path, publishDate)}`
   })
 
-  content = `${content}\n</sitemapindex>`
+  content = `${content}\n</urlset>`
   await writeFile('public/sitemap.xml', content, { encoding: 'utf-8' })
 }
 
